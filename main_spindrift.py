ad4ca4e605ff11bc8275c82bfc3f88c7df73642f
loverofspindrift2 <dylandrift481@gmail.com>
2025-04-29 16:00:26 -0700

    Update main_spindrift.py

--- main_spindrift.py
+++ main_spindrift.py
@@ -1,10 +1,10 @@
 def main():
   print("What is your name?")
   name = input()
-  print(f"Would you like a spindrift, {name}?")
+  print(f"Would you like a Spindrift, {name}?")
   y_n = input()
   if lower(y_n) == "y":
-    print("Very very good...")
+    print("Good...")
   else:
     raise ValueError("Incorrect Option")
 
